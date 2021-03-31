f32600dfb67c352d348f5c54f9d590cfe3382850
Kendal Montgomery <kmontgomery@covail.io>
2021-03-30 23:55:45 -0400

    updated to link GPO

--- configure_windows_event_forwarding.ps1
+++ configure_windows_event_forwarding.ps1
@@ -10,17 +10,20 @@ param ([String]$go)
 
 $domain_controller_policy_name = "ATDP_WindowsEventForwarding_DomainControllers"
 $subscription_manager_policy_key = "HKLM\Software\Policies\Microsoft\Windows\EventLog\EventForwarding\SubscriptionManager"
+$security_event_log_sd_key = "HKLM\Software\Policies\Microsoft\Windows\EventLog\Security"
 
 function Configure-DomainControllerEventFowarding {
   [CmdletBinding()]
   [OutputType([Boolean])]
   param()
 
+  ($hostobj = Get-WmiObject -Class Win32_ComputerSystem) 2>$null | out-null
   $dc_policy = (Get-Gpo -name $domain_controller_policy_name) 2>$null
 
   if (! $dc_policy) {
-    Write-Output "INFO: Policy ${domain_controller_policy_name} does not yet exist..."
-    $dc_policy = New-Gpo -Name ${domain_controller_policy_name} -Comment 'Windows Event Forwarding Policy for Domain Controllers for the COVAIL Attack Detector Platform' 2>$null
+    Write-Output "INFO: Policy ${domain_controller_policy_name} does not yet exist, creating new policy..."
+    $Error = $null
+    ($dc_policy = New-Gpo -Name "${domain_controller_policy_name}" -Comment 'Windows Event Forwarding Policy for Domain Controllers for the COVAIL Attack Detector Platform') | Out-Null
     if ($Error) {
       $m = $Error[-1].Exception.Message
       Write-Error "ERROR: Could not create policy ${domain_controller_policy_name}: ${m}."
@@ -31,11 +34,7 @@ function Configure-DomainControllerEventFowarding {
     Write-Output "INFO: Policy ${domain_controller_policy_name} exists!"
   }
 
-  $subscription_manager_policy = (Get-GPRegistryValue -Name "$domain_controller_policy_name" -Key "$subscription_manager_policy_key") 2>$null
-
-  if (! $subscription_manager_policy) {
-    Write-Output "INFO: Configuring WEF Subscritpion Manager Policy..."
-  }
+  ($subscription_manager_policy = Get-GPRegistryValue -Name "$domain_controller_policy_name" -Key "$subscription_manager_policy_key" 2>$null) | Out-Null
 
   if (! ((Test-Path -Path $PSScriptRoot\atdp_subscription_data.psd1) 2>$null) -eq $true) {
     Write-Warning "WARN: No susbscription configuration data exists, please follow the prompts to provide the required information..."
@@ -46,8 +45,44 @@ function Configure-DomainControllerEventFowarding {
 
   # $issuer_ca_thumbprint = (Get-ChildItem -Path cert:\LocalMachine\CA | where { $_.Subject -Match "CN=AtdpLab2-Root" } | foreach {$_.Thumbprint.ToLower()})
   # $domain_name = (Get-WmiObject Win32_ComputerSystem).Domain
-  $subscription = "Server=HTTPS://$($Config.WEC_Server_FQDN):5986/wsman/SubscriptionManager/WEC,Refresh=60,IssuerCA=$($Config.Auth_Certificate_Issuer_CA_Thumbprint)"
-  Write-Verbose "Subscription: ${subscription}"
+
+  if (! $subscription_manager_policy) {
+    Write-Output "INFO: Configuring WEF Subscritpion Manager Policy..."
+    $subscription = "Server=HTTPS://$($Config.WEC_Server_FQDN):5986/wsman/SubscriptionManager/WEC,Refresh=60,IssuerCA=$($Config.Auth_Certificate_Issuer_CA_Thumbprint)"
+    Write-Verbose "Subscription: ${subscription}"
+    ($subscription_manager_policy = Set-GPRegistryValue -Name "$domain_controller_policy_name" -Key "$subscription_manager_policy_key" -ValueName "1" -Type String -Value "${subscription}") | Out-Null
+  } else {
+    Write-Output "INFO: Subscription is currently set to: $($subscription_manager_policy.Value)"
+  }
+
+  # Configure security descriptor to allow NetworkService access to the Security Event Log
+
+  ($security_event_log_sd_policy = Get-GPRegistryValue -Name "$domain_controller_policy_name" -Key "$security_event_log_sd_key" 2>$null) | Out-Null
+  if (! $security_event_log_sd_policy) {
+    Write-Output "INFO: Configuring WEF Security Event Log SDDC Policy..."
+    $value = 'O:BAG:SYD:(A;;0xf0005;;;SY)(A;;0x5;;;BA)(A;;0x1;;;S-1–5–32–573)(A;;0x1;;;S-1–5–20)'
+    ($security_event_log_sd_policy = Set-GPRegistryValue -Name "$domain_controller_policy_name" -Key "$security_event_log_sd_key" -ValueName "ChannelAccess" -Type String -Value "${value}") | Out-Null
+  } else {
+    Write-Output "INFO: Security log SDDC already set to $($security_event_log_sd_policy.Value)"
+  }
+
+  # Ensure GPO is linked to Domain Controllers...
+  $ou = "ou=Domain Controllers,dc=$($hostobj.Domain.replace('.',',dc='))"
+
+  # Check for link
+  $LinkCount = (((Get-GPInheritance -Target "${ou}").GpoLinks | where { $_.DisplayName -eq "$domain_controller_policy_name" }).Length)
+
+  if ($LinkCount -lt 1) {
+    try {
+      Write-Output "INFO: linking ${domain_controller_policy_name} to ${ou}..."
+      ($link = New-GPLink -Name "$domain_controller_policy_name" -Target "${ou}" -Enforced Yes) | Out-Null
+    }
+    catch {
+      Write-Error "Could not create GPO Link: $_"
+    }
+  } else {
+    Write-Output "INFO: ${domain_controller_policy_name} is already linked at ${ou}.."
+  }
 
   return $true
 }
@@ -122,7 +157,7 @@ function Configure-ScriptData {
   WEC_Server_FQDN = "$($Config.WEC_Server_FQDN)"
   Auth_Certificate_Issuer_CA_Thumbprint = "$($Config.Auth_Certificate_Issuer_CA_Thumbprint)"
 }
-"@ | Out-File $PSScriptRoot\atdp_subscription_data.psd1 -Force
+"@ | Out-File $PSScriptRoot\atdp_subscription_data.psd1 -Encoding utf8 -Force
 
   return $true
 }
